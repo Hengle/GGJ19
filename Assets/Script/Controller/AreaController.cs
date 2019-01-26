b0c7af57a4de6978e0a6b2b83045cdd4d2f56d71
Utku Sipahioğlu <utku.sipahioglu@siis.com.tr>
2019-01-26 21:17:42 +0300

    - build ayarları güncellendi. - sahne bölgelere ayrıldı (sol, tarafsız, sağ). - Bilge ve Skor için kontrolcüler eklendi.

---
+++ Assets/Script/Controller/AreaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaController : MonoBehaviour
+{
+	public Area                  area;
+	public List<StuffController> stuffs;
+	public ScoreController       SC;
+
+	public void Add(StuffController newSC)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void Remove(StuffController newSC)
+	{
+		throw new NotImplementedException();
+	}
+
+	public int CountStuffs()
+	{
+		return 0;
+	}
+}
+
+public enum Area
+{
+	Neutral,
+	Left,
+	Right
+}
