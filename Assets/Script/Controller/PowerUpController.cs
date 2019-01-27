1d518495d991c02ba79d7fa15174fc276246db4f
Utku Sipahioğlu <utku.sipahioglu@siis.com.tr>
2019-01-27 15:56:05 +0300

    -powerup'lar görselleştirildi.

--- Assets/Script/Controller/PowerUpController.cs
+++ Assets/Script/Controller/PowerUpController.cs
@@ -5,65 +5,122 @@ using Random = UnityEngine.Random;
 
 public class PowerUpController : MonoBehaviour
 {
-    public PowerUpType type;
-    private TransformFunctions TF;
-    private PowerUpManager PUM;
-    public Transform targetScale;
-    public AnimationCurve destroyCurve;
+	public  PowerUpType        type;
+	private TransformFunctions TF;
+	private PowerUpManager     PUM;
+	public  Transform          targetScale;
+	public  AnimationCurve     destroyCurve;
 
-    [Range(5f, 120f)] public float destroyAfter = 12f;
+	public Sprite         staminaSprite;
+	public Sprite         speedSprite;
+	public Sprite         heavierSprite;
+	public SpriteRenderer spriteRenderer;
 
-    private float passedTime;
+	public Sprite         stamina2Sprite;
+	public Sprite         speed2Sprite;
+	public Sprite         heavier2Sprite;
+	public SpriteRenderer sprite2Renderer;
 
-    void Start()
-    {
-        type = RandomEnumValue<PowerUpType>();
-        TF = FindObjectOfType<TransformFunctions>();
-        PUM = FindObjectOfType<PowerUpManager>();
-        targetScale = GameObject.Find("ZeroScale").transform;
-    }
+	public Sprite         stamina3Sprite;
+	public Sprite         speed3Sprite;
+	public Sprite         heavier3Sprite;
+	public SpriteRenderer sprite3Renderer;
 
-    private void LateUpdate()
-    {
-        if (passedTime >= destroyAfter)
-        {
-            DestroyThis();
-            passedTime = -1000f;
-        }
 
-        passedTime += Time.deltaTime;
-    }
+	public  AudioClip   staminaClip;
+	public  AudioClip   speedClip;
+	public  AudioClip   heavierClip;
+	private AudioSource audioSource;
 
-    public void Use(PlayerController player)
-    {
-        PUM.PowerUp(type, player);
-        DestroyThis();
-    }
+	[Range(5f, 120f)] public float destroyAfter = 12f;
 
-    void DestroyThis()
-    {
-        GetComponent<Collider>().enabled = false;
-        StartCoroutine(_Destroy());
-        TF.Scale(transform, targetScale, 0f, 0.33f, destroyCurve);
-    }
+	private float passedTime;
 
-    IEnumerator _Destroy()
-    {
-        yield return new WaitForSeconds(0.4f);
-        Destroy(gameObject);
-    }
+	void Start()
+	{
+		type = RandomEnumValue<PowerUpType>();
 
-    T RandomEnumValue<T>()
-    {
-        var v = Enum.GetValues(typeof(T));
-        return (T)v.GetValue(Random.Range(0, v.Length));
-    }
+		audioSource = FindObjectOfType<AudioSource>();
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            Use(other.GetComponent<PlayerController>());
-        }
-    }
+		switch(type)
+		{
+			case PowerUpType.Stamina:
+				spriteRenderer.sprite  = staminaSprite;
+				sprite3Renderer.sprite = stamina3Sprite;
+				sprite2Renderer.sprite = stamina2Sprite;
+				break;
+			case PowerUpType.Heavier:
+				spriteRenderer.sprite  = heavierSprite;
+				sprite3Renderer.sprite = heavier3Sprite;
+				sprite2Renderer.sprite = heavier2Sprite;
+				break;
+			case PowerUpType.Speed:
+				spriteRenderer.sprite  = speedSprite;
+				sprite3Renderer.sprite = speed3Sprite;
+				sprite2Renderer.sprite = speed2Sprite;
+				break;
+		}
+
+		TF          = FindObjectOfType<TransformFunctions>();
+		PUM         = FindObjectOfType<PowerUpManager>();
+		targetScale = GameObject.Find("ZeroScale").transform;
+	}
+
+	private void LateUpdate()
+	{
+		if(passedTime >= destroyAfter)
+		{
+			DestroyThis();
+			passedTime = -1000f;
+		}
+
+		passedTime += Time.deltaTime;
+	}
+
+	public void Use(PlayerController player)
+	{
+		switch(type)
+		{
+			case PowerUpType.Stamina:
+				audioSource.clip = staminaClip;
+				break;
+			case PowerUpType.Heavier:
+				audioSource.clip = heavierClip;
+				break;
+			case PowerUpType.Speed:
+				audioSource.clip = speedClip;
+				break;
+		}
+
+		audioSource.PlayOneShot(audioSource.clip);
+		PUM.PowerUp(type, player);
+		DestroyThis();
+	}
+
+	void DestroyThis()
+	{
+		GetComponent<Collider>().enabled = false;
+		StartCoroutine(_Destroy());
+		TF.Scale(transform, targetScale, 0f, 0.33f, destroyCurve);
+	}
+
+	IEnumerator _Destroy()
+	{
+		yield return new WaitForSeconds(2.5f);
+		Destroy(gameObject);
+	}
+
+	T RandomEnumValue<T>()
+	{
+		var v = Enum.GetValues(typeof(T));
+		return (T) v.GetValue(Random.Range(0, v.Length));
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("Player"))
+		{
+			Use(other.GetComponent<PlayerController>());
+		}
+	}
 }
